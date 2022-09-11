210ae61730b3fe8b842d14497d43efa3ba0cc4fb
= <=>
2022-09-11 20:14:34 -0300

    adjust db configs

--- src/4-framework/const/databaseConfigs.ts
+++ src/4-framework/const/databaseConfigs.ts
@@ -3,32 +3,21 @@ import path from 'path'
 import { config } from 'dotenv'
 config()
 
-let database: string
-let host: string
-let username: string
-let password: string
+let host = process.env.DB_HOST as string
 
-if (!process.env.CLEARDB_DATABASE_URL) {
-  database = process.env.DB_LOCAL as string
-  host = process.env.DB_HOST_LOCAL as string
-  username = process.env.DB_USER_LOCAL as string
-  password = process.env.DB_PASS_LOCAL as string
-} else {
+if (process.env.CLEARDB_DATABASE_URL) {
   const clearDBFullUrl = process.env.CLEARDB_DATABASE_URL
 
   const [, urlWithDataBase] = clearDBFullUrl?.split('@') as string[]
 
-  database = process.env.DB as string
   host = urlWithDataBase.split('/')[0]
-  username = process.env.DB_USER as string
-  password = process.env.DB_PASS as string
 }
 
 export const baseSequelizeOptions = {
-  database,
   host,
-  username,
-  password,
+  database: process.env.DB_NAME as string,
+  username: process.env.DB_USER as string,
+  password: process.env.DB_PASS as string,
   storage: ':memory:',
   models: [path.resolve(__dirname, '..', 'models', 'mysql', '*.model.ts')]
 } as SequelizeOptions
