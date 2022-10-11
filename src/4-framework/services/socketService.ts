d0c7f7d949619cd7a97d62419f2ccc6b056e89fd
lf-lima <luizfelipe5007@gmail.com>
2022-10-11 09:28:15 -0300

    add disconnection of user in socket

--- src/4-framework/services/socketService.ts
+++ src/4-framework/services/socketService.ts
@@ -1,5 +1,5 @@
 import { ISocketService } from '#business/services/iSocketService'
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 
 export class SocketService implements ISocketService {
   private io!: Server
@@ -9,15 +9,26 @@ export class SocketService implements ISocketService {
   }
 
   init (): void {
+    const usersOnline: string[] = []
     const messages: any[] = ['Testando mensagem']
 
     this.io.on('connection', socket => {
+      console.log(`new user online: ${socket.id}`)
+      usersOnline.push(socket.id)
+
+      socket.on('disconnect', () => {
+        console.log(`user ${socket.id} disconnect`)
+
+        const i = usersOnline.indexOf(socket.id)
+        usersOnline.splice(i, 1)
+      })
+
       socket.emit('message:render-olds', messages)
 
       let username = ''
       socket.on('user:enter', (_username) => {
         username = _username
-        console.log(`New user: ${username}`)
+        console.log(`enter user: ${username}`)
       })
 
       socket.on('message:send', (message) => {
